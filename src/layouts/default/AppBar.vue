c239999b6930d2ab3d37b3dc1e320c83de2a47b7
konishi <konishi@tdssap.co.jp>
2023-09-01 22:49:43 +0900

    メニュー表示切替

--- src/layouts/default/AppBar.vue
+++ src/layouts/default/AppBar.vue
@@ -1,13 +1,13 @@
 <template>
   <v-app-bar flat>
-    <v-app-bar-nav-icon variant="text" @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
+    <v-app-bar-nav-icon v-if="$vuetify.display.xs" variant="text" @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
     <v-app-bar-title> v-app-bar-title </v-app-bar-title>
-    <v-tabs>
+    <v-tabs v-if="$vuetify.display.smAndUp">
       <v-tab v-for="col in menuItem" :to=col[1] exact> {{ col[0] }} </v-tab>
     </v-tabs>
   </v-app-bar>
 
-  <v-navigation-drawer v-model="drawer">
+  <v-navigation-drawer v-if="$vuetify.display.xs" v-model="drawer">
     <v-list-item v-for="col in menuItem" @click.stop="drawer = false" :to=col[1] exact> {{ col[0] }} </v-list-item>
   </v-navigation-drawer>
 </template>
